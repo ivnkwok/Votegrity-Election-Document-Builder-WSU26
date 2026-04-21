55029b18e92bfb01413be80fcb192e2f6775c531
Ivan Kwok <ivnkwok@gmail.com>
2026-04-21 15:35:27 -0700

    implemented initial api call
    @TiengLance hurry up

---
+++ code/src/services/apiService.ts
@@ -0,0 +1,7 @@
+export async function fetchElectionData(url: string): Promise<any> {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.status}`)
+    }
+    return await response.json();
+}
